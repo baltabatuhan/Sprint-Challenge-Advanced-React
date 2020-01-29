05f03905775909d3e70008012576794681cba393
Batuhan Balta <batuhanbalta@Batuhans-MacBook-Pro.local>
2020-01-28 21:27:03 -0700

    mvp met :)

---
+++ client/src/components/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { useDarkMode } from "../hooks/useDarkMode";
+
+const Header = () => {
+  const toggleMode = e => {
+    e.preventDefault();
+    setDarkMode(!darkMode);
+  };
+  const [darkMode, setDarkMode] = useDarkMode(toggleMode);
+
+  return (
+    <div className="navbar">
+      <button className="theme-button" onClick={toggleMode}>
+        Change Theme
+      </button>
+    </div>
+  );
+};
+
+export default Header;
